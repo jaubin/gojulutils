ddea9ad7959b96bf434b41a6f7446fe49ed481fe
Julien Aubin <julien_aubin_lm@yahoo.fr>
2016-02-15 18:38:09 +0100

    refactoring

---
+++ src/main/java/org/gojul/gojulutils/safetools/GojulEncapsulationUtils.java
@@ -0,0 +1,71 @@
+package org.gojul.gojulutils.safetools;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Class {@code GojulEncapsulationUtils} contains safe methods to help you
+ * encapsulate your object instances by returning copies of objects instead
+ * of objects themselves. The goal here is notably to allow you to remove
+ * {@code null} checks prior to doing defensive copy.
+ * 
+ * @author julien
+ *
+ */
+public class GojulEncapsulationUtils {
+
+	/**
+	 * Private constructor. Prevents class
+	 * from being instanciated.
+	 */
+	private GojulEncapsulationUtils() {
+		
+	}
+	
+	/**
+	 * Return an unmodifiable {@link List} around {@code l},
+	 * or {@code null} if {@code l} is {@code null}.
+	 * @param l the {@link List} to wrap.
+	 * @return n unmodifiable {@link List} around {@code l},
+	 * or {@code null} if {@code l} is {@code null}.
+	 */
+	public static <T> List<T> unmodifiableList(final List<T> l) {
+		return l == null ? null: Collections.unmodifiableList(l);
+	}
+	
+	/**
+	 * Return an unmodifiable {@link Set} around {@code s},
+	 * or {@code null} if {@code s} is {@code null}.
+	 * @param s the {@link Set} to wrap.
+	 * @return an unmodifiable {@link Set} around {@code s},
+	 * or {@code null} if {@code s} is {@code null}.
+	 */
+	public static <T> Set<T> unmodifiableSet(final Set<T> s) {
+		return s == null ? null: Collections.unmodifiableSet(s);
+	}
+		
+	/**
+	 * Return an unmodifiable {@link Collection} around {@code c},
+	 * or {@code null} if {@code c} is {@code null}.
+	 * @param c the {@link Collection} to wrap.
+	 * @return an unmodifiable {@link Collection} around {@code c},
+	 * or {@code null} if {@code c} is {@code null}.
+	 */
+	public static <T> Collection<T> unmodifiableCollection(final Collection<T> c) {
+		return c == null ? null: Collections.unmodifiableCollection(c);
+	}
+	
+	/**
+	 * Return an unmodifiable {@link Map} around {@code m},
+	 * or a {@code null} if {@code m} is {@code null}.
+	 * @param m the {@link Map} to wrap.
+	 * @return an unmodifiable {@link Map} around {@code m},
+	 * or a {@code null} if {@code m} is {@code null}.
+	 */
+	public static <K, V> Map<K, V> unmodifiableMap(final Map<K, V> m) {
+		return m == null ? null: Collections.unmodifiableMap(m);
+	}
+}
---
+++ src/test/java/org/gojul/gojulutils/safetools/GojulEncapsulationUtilsTest.java
@@ -0,0 +1,55 @@
+package org.gojul.gojulutils.safetools;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class GojulEncapsulationUtilsTest {
+
+	@Test
+	public void testUnmodifiableList() {
+		assertNull(GojulEncapsulationUtils.unmodifiableList(null));
+		List<String> expected = Arrays.asList("foo", "bar"); 
+		assertEquals(expected, GojulEncapsulationUtils.unmodifiableList(expected));
+		assertNotSame(expected, GojulEncapsulationUtils.unmodifiableList(expected));
+	}
+
+	@Test
+	public void testUnmodifiableSet() {
+		assertNull(GojulEncapsulationUtils.unmodifiableSet(null));
+		Set<String> expected = new HashSet<>(Arrays.asList("foo", "bar")); 
+		assertEquals(expected, GojulEncapsulationUtils.unmodifiableSet(expected));
+		assertNotSame(expected, GojulEncapsulationUtils.unmodifiableSet(expected));
+	}
+	
+	@Test
+	public void testUnmodifiableCollection() {
+		assertNull(GojulEncapsulationUtils.unmodifiableCollection(null));
+		List<String> expected = Arrays.asList("foo", "bar");
+		
+		Collection<String> found = GojulEncapsulationUtils.unmodifiableCollection(Arrays.asList("foo", "bar"));
+		assertEquals(expected.size(), found.size());
+		assertTrue(expected.containsAll(found));
+		assertTrue(found.containsAll(expected));
+		assertNotSame(found, expected);
+	}
+	
+	@Test
+	public void testUnmodifiableMap() {
+		assertNull(GojulEncapsulationUtils.unmodifiableMap(null));
+		
+		Map<String, String> expected = new HashMap<>();
+		expected.put("foo", "bar");
+		expected.put("hello", "world");
+		assertEquals(expected, GojulEncapsulationUtils.unmodifiableMap(expected));
+		assertNotSame(expected, GojulEncapsulationUtils.unmodifiableMap(expected));
+	}
+}
